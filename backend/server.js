f4ebf49a9733bda595c9b26a625d522ab38c0d86
Hamza-Yousaf <hamzayousaf0786@gmail.com>
2025-05-29 18:36:17 +0500

    cors auth and loginpage - mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import taskRoutes from './routes/taskRoutes.js'
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+  const [newUser, setNewUser] = useState({
+    name: "",
+    role: "",
+    position: "",
+    email: ""
+  });
+
+  const handleAddUser = async () => {
+    if(!newUser.name || !newUser.role || !newUser.position || !newUser.email) {
+      console.log("please proivde all fields");
+      return {success: false, message: "Please provide all fields"}
+    }
+
+    const res = await fetch("http://localhost:5000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(newUser)
+    })
+    
+    const data = await res.json();
+  }
+
   return (
     // <div>loginPage</div>
 
@@ -10,13 +34,27 @@ const LoginPage = () => {
                 <div className="flex flex-col">
                     <h1 className='text-xl text-gray-900 mb-5 text-white'>Sign Up to <span className='font-bold text-orange-200'>DevRequest</span></h1>
 
-                    <input type="text" placeholder='Username' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'/>
-                    <input type="text" placeholder='Role' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'/>
-                    <input type="text" placeholder='Position' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'/>
-                    <input type="text" placeholder='Email' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'/>
+                    <input type="text" placeholder='Username' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'
+                    value={newUser.name}
+                    onChange={(e) => setNewUser({ ...newUser, name: e.target.value})}/>
+
+                    <input type="text" placeholder='Role' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'
+                    value={newUser.role}
+                    onChange={(e) => setNewUser({ ...newUser, role: e.target.value})}/>
+
+                    <input type="text" placeholder='Position' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'
+                    value={newUser.position}
+                    onChange={(e) => setNewUser({ ...newUser, position: e.target.value})}/>
+
+                    <input type="text" placeholder='Email' className='relative w-full mt-4 p-2 bg-white rounded-sm border-2 border-gray-300 focus:border-orange-200 focus:border-2 focus:outline-none'
+                    value={newUser.email}
+                    onChange={(e) => setNewUser({ ...newUser, email: e.target.value})}/>
                 </div>
 
-                <button className='bg-gray-900 w-full h-auto p-2 rounded-sm text-white'>Submit</button>
+                <button className='bg-gray-900 w-full h-auto p-2 rounded-sm text-white cursor-pointer'
+                onClick={handleAddUser}>
+                  Submit
+                </button>
             </div>
         </div>
     </div>
